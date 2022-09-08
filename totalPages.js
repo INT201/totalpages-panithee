684b49c593bf09f71b802f3c854209a1fefcf5dc
panithee <panithee.pnt@mail.kmutt.ac.th>
2022-09-08 23:39:44 +0700

    Finished totalPages

--- totalPages.js
+++ totalPages.js
@@ -2,5 +2,15 @@ const { template } = require('@babel/core')
 
 function totalPages(arrayItems, rowsPerPage) {
   //code here
+  if(arrayItems === undefined || arrayItems === null){
+    return undefined;
+  } 
+  else if(rowsPerPage === undefined ||  rowsPerPage === null || rowsPerPage===0 ){
+    return 1;
+  }
+  return Math.ceil(arrayItems.length / rowsPerPage)
+ 
+
+  
 }
 module.exports = totalPages
